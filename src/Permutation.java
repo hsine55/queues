1594f877c44a476b60927cf6734713d34b22056c
HassineBM <hsine.benmessaoud@gmail.com>
2017-03-10 15:06:04 +0100

    Update Permutation.java
    Permutation implemented

--- src/Permutation.java
+++ src/Permutation.java
@@ -1,8 +1,20 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
 
-public class Permutation {
+public class Permutation
+{
 
-	public static void main(String[] args) {
-
-	}
-
+    public static void main(String[] args)
+    {
+        Integer k = Integer.valueOf(args[0]);
+        RandomizedQueue rq = new RandomizedQueue();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            rq.enqueue(s);
+        }
+        while(k>0) {
+            System.out.println(rq.dequeue());
+            k--;
+        }
+    }
 }
